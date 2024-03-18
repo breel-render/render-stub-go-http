dc88b6107867fdc6aef9585beed81e2627c6ae58
Bel LaPointe <153096461+breel-render@users.noreply.github.com>
2024-03-18 15:45:42 -0600

    some env configs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module render-stub-go-http
 
 go 1.21.4
+
+require golang.org/x/time v0.5.0
--- main.go
+++ main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
-	"time"
+	"os"
+	"strconv"
+
+	"golang.org/x/time/rate"
 )
 
+var (
+	Listen = envOr("LISTEN", ":8080")
+	RPS    = mustFloat(envOr("RPS", "3"))
+)
+
+func envOr(k, v string) string {
+	if v2 := os.Getenv(k); v2 != "" {
+		return v2
+	}
+	return v
+}
+
+func mustFloat(s string) float64 {
+	if v, err := strconv.ParseFloat(s, 64); err == nil {
+		return v
+	}
+	if v, err := strconv.ParseInt(s, 10, 32); err == nil {
+		return float64(v)
+	}
+	panic(fmt.Errorf("%s is not a float", s))
+}
+
 func main() {
-	next := time.Now()
-	if err := http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if toWait := next.Sub(time.Now()); toWait > 0 {
-			time.Sleep(toWait)
-		}
-		next = time.Now().Add(time.Second)
-		w.Write([]byte("hello world"))
+	limiter := rate.NewLimiter(rate.Limit(RPS), 1)
+	if err := http.ListenAndServe(Listen, http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		limiter.Wait(r.Context())
+		headers, _ := json.MarshalIndent(r.Header, "   ", "   ")
+		body, _ := io.ReadAll(r.Body)
+		fmt.Fprintf(w, "%s %s\n%s\n   %s\n",
+			r.Method, r.URL,
+			headers,
+			body,
+		)
 	})); err != nil {
 		panic(err)
 	}
